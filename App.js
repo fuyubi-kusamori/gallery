6c19902134cbc817dc36b2e9846966bd918644ec
iammyowncliche <iammyowncliche@gmail.com>
2024-04-19 00:15:58 +0900

    Change img

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Gallery from './Gallery';
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Simple Gallery</h1>
+      <Gallery />
+    </div>
+  );
+};
+
+export default App;
