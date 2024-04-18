fe02861ce173b9ae61e8bcc505670d539e878920
arietta <165660062+fuyubi-kusamori@users.noreply.github.com>
2024-04-19 01:17:31 +0900

    Update Gallery.jsx

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -5,7 +5,7 @@ const Gallery = () => {
   const generateImageData = (numImages) => {
     const images = [];
     for (let i = 1; i <= numImages; i++) {
-      images.push({ id: i, url: `https://gallery-rouge-rho.vercel.app/public/img/${i}_s.jpg` });
+      images.push({ id: i, url: `https://gallery-rouge-rho.vercel.app/img/${i}_s.jpg` });
     }
     return images;
   };
